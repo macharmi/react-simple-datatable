9bcb51947205e727e90efc092b2344625d3b020a
Mohamed Ali <medali@yusofleet.com>
2019-04-14 19:16:29 +0100

    add pagination

--- src/dataTable.js
+++ src/dataTable.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSort, faSortUp, faSortDown } from '@fortawesome/free-solid-svg-icons'
 import SortIcon from './components/sortIcon'
+import {Pagination, PaginationItem, PaginationLink} from 'reactstrap'
 
 library.add(faSort, faSortUp, faSortDown)
 
@@ -14,14 +15,16 @@ class DataTable extends Component{
         this.state = {
             sortCriteria: {},
             currentPage: 1,
-            perPage: 10
+            perPage: 10,
+            data: props.data
         }
     }
 
     render(){
         let searchCriteria = []
         let sortCriteria = {}
-        const raiseSearchEvent = (key, value) => {
+
+        const handleSearch = (key, value) => {
             searchCriteria = [...searchCriteria, {key: key, value: value}]
             searchCriteria = searchCriteria.filter((e) => {
                 const sameKey = e.key === key && e.value !== value
@@ -32,7 +35,7 @@ class DataTable extends Component{
                 this.props.onSearch(searchCriteria)
         }
 
-        const raiseSortEvent = (key) => {
+        const handleSort = (key) => {
             if(this.state.sortCriteria.key === key)
                 sortCriteria = {key: key, order: (this.state.sortCriteria.order + 1) % 3}
             else
@@ -41,19 +44,42 @@ class DataTable extends Component{
             if(this.props.onSort)
                 this.props.onSort(sortCriteria)
         }
+        const handlePagination = (page) => {
+            onPaginate(this.props.data, page, 10)
+        }
+
+        const onPaginate = (data, pageNumber, pageSize) => {
+            --pageNumber;
+            this.setState({...this.state, data: data.slice(pageNumber * pageSize, (pageNumber + 1) * pageSize)});
+        }
+
+        const pagination = () => {
+            const recordCount = this.props.rowNumber || this.props.data.length
+            return(
+                <Pagination>
+                    <PaginationItem>
+                        <PaginationLink first href="#" onClick={() => {handlePagination(1)}}>First</PaginationLink>
+                    </PaginationItem>
+                    {[...Array(Math.ceil(recordCount/10))].map((_,i) => i + 1).map((page) => (<PaginationItem><PaginationLink onClick={() => {handlePagination(page)}} href="#">{page}</PaginationLink></PaginationItem>))}
+                    <PaginationItem>
+                        <PaginationLink last href="#" onClick={() => {handlePagination(Math.ceil(recordCount/10))}}>Last</PaginationLink>
+                    </PaginationItem>
+                </Pagination>
+             )
+        }
 
         const tableHead = this.props.columns.map(
             (column) => {
                 if(column.sortable)
                     return(
                         <th >
-                            <a  name="" 
-                                onClick={() => {raiseSortEvent(column.key)}
+                            <a
+                                href="#"
+                                onClick={() => {handleSort(column.key)}
                             }>
                                 {column.name}
-                            </a>
                             <SortIcon searchable={column.searchable} order={this.state.sortCriteria.key === column.key?this.state.sortCriteria.order:""}></SortIcon>
-                            {" "}{}
+                            </a>
                         </th>
                     )
                 else
@@ -64,14 +90,14 @@ class DataTable extends Component{
         const searchFields = this.props.columns.map(
             (column) => {
                 if(column.searchable)
-                    return(<td><input className='form-control' name={column.key} onChange={(e) => raiseSearchEvent(e.target.name, e.target.value)}></input></td>)
+                    return(<td><input className='form-control' name={column.key} onChange={(e) => handleSearch(e.target.name, e.target.value)}></input></td>)
                 else
                     return(<td></td>)
 
             }
         )
 
-        const tableBody = this.props.data.map(
+        const tableBody = this.state.data.map(
             (dataLine) => {
                 const rowData = this.props.columns.map(
                     (column) => {
@@ -87,6 +113,7 @@ class DataTable extends Component{
 
         return(
             <div>
+                {pagination()}
                 <h1>{this.props.title}</h1>
                 <table className={this.props.className}>
                     <thead>
@@ -101,6 +128,7 @@ class DataTable extends Component{
                         {tableBody}
                     </tbody>
                 </table>
+
             </div>
         )
     }
